20840893f2958fb8b1cda907365531050fbf1bd2
Spellborn <keryakerya1@gmail.com>
2018-09-12 19:28:06 +0300

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe PagesController, type: :controller do
   describe "GET #home" do
     it "returns http success" do
       get :home
+       #response.should be_success
       expect(response).to have_http_status(:success)
     end
   end
